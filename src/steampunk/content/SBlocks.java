63770e6112989a3b5bfb5c44887d964ad1e1594e
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-03-02 22:34:09 +0900

    h

--- src/steampunk/content/SBlocks.java
+++ src/steampunk/content/SBlocks.java
@@ -1,10 +1,15 @@
 package steampunk.content;
 
 import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.Liquids;
 import mindustry.ctype.ContentList;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.production.GenericCrafter;
 import mindustry.world.blocks.production.GenericSmelter;
+import multilib.*;
+import multilib.Recipe.*;
 
 import static mindustry.type.ItemStack.with;
 
@@ -17,8 +22,25 @@ public class SBlocks implements ContentList {
     @Override
     public void load() {
         //region production
-        formwork = new GenericSmelter ("formwork"){{
+        formwork = new MultiCrafter("formwork", 3){{
             requirements(Category.crafting, with(SItems.wood, 20, SItems.stone, 20, SItems.clay, 10));
+            size = 2;
+            isSmelter = true;
+            addRecipe(
+                    new InputContents (with(SItems.tin, 1, Items.copper, 3)),
+                    new OutputContents(with(SItems.bronze, 2)), 60f
+            );
+
+            addRecipe(
+                    new InputContents(with(SItems.wood, 2)),
+                    new OutputContents(with(Items.coal, 1)), 40f
+            );
+
+            addRecipe(
+                    new InputContents(with(SItems.dirt, 2, Liquids.water, 3)),
+                    new OutputContents(with(SItems.clay, 1)), 90f
+
+            );
 
             craftEffect = Fx.burning;
 
--- src/steampunk/content/SItems.java
+++ src/steampunk/content/SItems.java
@@ -5,7 +5,11 @@ import mindustry.ctype.ContentList;
 import mindustry.type.Item;
 
 public class SItems implements ContentList {
-    public static Item wood, stone, clay;
+    public static Item
+    //stone age
+    wood, stone, dirt, clay,
+    //bronze age
+    tin, bronze;
 
     @Override
     public void load(){
@@ -15,5 +19,36 @@ public class SItems implements ContentList {
             alwaysUnlocked = true;
 
         }};
+
+        stone = new Item("wood", Color.gray){{
+            hardness = 0;
+            cost = 0.2f;
+            alwaysUnlocked = true;
+        }};
+
+        dirt = new Item("dirt", Color.valueOf("9b7653")){{
+            hardness = 0;
+            cost = 0.1f;
+            alwaysUnlocked = true;
+        }};
+
+        clay = new Item("clay", Color.lightGray){{
+            hardness = 0;
+            flammability = 0.3f;
+            cost = 0.1f;
+            alwaysUnlocked = false;
+        }};
+
+        tin = new Item("tin", Color.valueOf("d3d4d5")){{
+            hardness = 2;
+            cost = 0.3f;
+            alwaysUnlocked = false;
+        }};
+
+        bronze = new Item("bronze", Color.valueOf("cd7f32")){{
+            hardness = 3;
+            cost = 0.5f;
+        }};
+
     }
 }
