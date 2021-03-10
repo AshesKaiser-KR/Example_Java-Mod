5314fae74a32c4cc5371f47f23dbf6e3b85a1931
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-03-10 09:27:36 +0900

    Update SBlocks.java

--- src/steampunk/content/SBlocks.java
+++ src/steampunk/content/SBlocks.java
@@ -1,11 +1,10 @@
 package steampunk.content;
 
-import mindustry.content.Fx;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
+import mindustry.content.*;
 import mindustry.ctype.ContentList;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.environment.Floor;
 import mindustry.world.blocks.production.GenericCrafter;
 import mindustry.world.blocks.production.GenericSmelter;
 import multilib.*;
@@ -23,6 +22,20 @@ public class SBlocks implements ContentList {
 
     @Override
     public void load() {
+        //region environment
+        soil = new Floor("soil"){{
+            itemDrop = SItems.wood;
+            playerUnmineable = false;
+            status = StatusEffects.wet;
+            statusDuration = 90f;
+            speedMultiplier = 0.8f;
+        }};
+
+        Blocks.stone = new Floor("stone"){{
+            itemDrop = SItems.stone;
+            playerUnmineable = false;
+        }};
+
         //region production
         formwork = new MultiCrafter("formwork", 3){{
             requirements(Category.crafting, with(SItems.wood, 20, SItems.stone, 20, SItems.clay, 10));
